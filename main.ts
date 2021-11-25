b69c37131b4b2354fede777781478e477984f5a2
Kaems51 <91282172+Kaems51@users.noreply.github.com>
2021-11-25 19:32:26 +0100

    De første filer til MakeCode-projekt

---
+++ main.ts
@@ -0,0 +1,13 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.Sad)
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.showIcon(IconNames.Asleep)
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showIcon(IconNames.Happy)
+})
+basic.showIcon(IconNames.Asleep)
+basic.forever(function () {
+	
+})
